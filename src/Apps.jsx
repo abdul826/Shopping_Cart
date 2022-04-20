bac465871b9fa727f03377efb412badb4509ee55
abdul826 <59992187+abdul826@users.noreply.github.com>
2022-04-20 22:44:05 +0530

    Add files via upload

---
+++ src/Apps.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style/Cart.css'
+import Cart from './Components/Cart';
+
+const Apps = () => {
+  return (
+    <>
+      <Cart />
+    </>
+  )
+}
+
+export default Apps
